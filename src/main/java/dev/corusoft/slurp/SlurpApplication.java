7ea2710a5825fc1bef1b78fc9bc57d46b03db3cf
Javier Cancela <javier.cmato@udc.es>
2023-10-16 10:03:21 +0200

    [PROJECT] Initial project setup

---
+++ src/main/java/dev/corusoft/slurp/SlurpApplication.java
@@ -0,0 +1,14 @@
+package dev.corusoft.slurp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SlurpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlurpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/corusoft/slurp/SlurpApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.corusoft.slurp;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlurpApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true);
+    }
+
+}
