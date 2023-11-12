4f2a144c7a4d9bdd5a064b30aed4a7a73aafb1f5
Javier Cancela <javiciier@gmail.com>
2023-11-12 16:15:09 +0100

    [USER] Implement change user password
    - Remove @UtilityClass from lombok

--- src/main/java/dev/corusoft/slurp/common/security/SecurityConstants.java
+++ src/main/java/dev/corusoft/slurp/common/security/SecurityConstants.java
@@ -1,14 +1,12 @@
 package dev.corusoft.slurp.common.security;
 
-import lombok.experimental.UtilityClass;
 
-@UtilityClass
 public class SecurityConstants {
     /* HTTP */
-    public final String PREFIX_BEARER_TOKEN = "Bearer ";
+    public static final String PREFIX_BEARER_TOKEN = "Bearer ";
 
     /* AUTHENTICATION */
-    public final String TOKEN_ATTRIBUTE_NAME = "service_token";
-    public final String USER_ID_ATTRIBUTE_NAME = "userID";
-    public final String ROLE_ATTRIBUTE_NAME = "ROLE_";
+    public static final String TOKEN_ATTRIBUTE_NAME = "service_token";
+    public static final String USER_ID_ATTRIBUTE_NAME = "userID";
+    public static final String ROLE_ATTRIBUTE_NAME = "ROLE_";
 }
